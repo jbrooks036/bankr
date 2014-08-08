b07239c9e3fd585b022faebc927556c568b96005
JoAnn Brooks <joannbrooks036@gmail.com>
2014-08-08 15:04:39 -0500

    initial setup

---
+++ db/account.js
@@ -0,0 +1,4 @@
+{"_id":{"$oid":"53d01ddf4fbbd6de0b530014"}, "name":"Low",    "color":"#11cc33", "value":3}
+{"_id":{"$oid":"53d01ddf4fbbd6de0b530015"}, "name":"Medium", "color":"#22cc33", "value":5}
+{"_id":{"$oid":"53d01ddf4fbbd6de0b530016"}, "name":"High",   "color":"#33cc33", "value":7}
+
